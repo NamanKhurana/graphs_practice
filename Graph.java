585a3f98568c5e56c04dcdfcd501c30429ca8822
NamanKhurana <khurananaman06@gmail.com>
2020-09-17 14:03:31 +0530

    LEETCODE-463 ques added

--- Graph.java
+++ Graph.java
@@ -115,48 +115,47 @@ public class Graph {
     return totalServers;
 }
 
- vector<vector<int>> floodDir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-public:
-    bool isValidIsl(int sr, int sc, int er, int ec, vector<vector<char>> &grid)
-{
-    if (sr < 0 || sr > er || sc < 0 || sc > ec || grid[sr][sc] == '2' || grid[sr][sc] == '0')
-    {
-        return false;
-    }
-    return true;
-}
-
-void dfs(int sr,int sc,int er,int ec,vector<vector<char>> &grid){
-    
-    grid[sr][sc] = '2';
-    int count = 0;
-    for (int i = 0; i < floodDir.size(); i++)
-    {
-        // for(int jump = 1;jump<=3;jump++){
-        int nr = sr + floodDir[i][0];
-        int nc = sc + floodDir[i][1];
-        if (isValidIsl(nr, nc, er, ec, grid))
-        {
-            dfs(nr, nc, er, ec, grid);
-        }
-        // }
-    }
-}
-
 //LEETCODE 200
-int numIslands(vector<vector<char>> &grid)
-{
+public int numIslands(char[][] grid) {
     int count = 0;
-    for(int i = 0;i<grid.size();i++){
-        for(int j = 0;j<grid[0].size();j++){
-            if(grid[i][j] == '1'){
-                count++;
-                dfs(i,j,grid.size()-1,grid[0].size()-1,grid);
+    n = grid.length;
+    if (n == 0) return 0;
+    m = grid[0].length;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++)
+            if (grid[i][j] == '1') {
+                DFSMarking(grid, i, j);
+                ++count;
             }
-        }
-    }
+    }    
     return count;
 }
+
+private void DFSMarking(char[][] grid, int i, int j) {
+    if (i < 0 || j < 0 || i >= n || j >= m || grid[i][j] != '1') return;
+    grid[i][j] = '0';
+    DFSMarking(grid, i + 1, j);
+    DFSMarking(grid, i - 1, j);
+    DFSMarking(grid, i, j + 1);
+    DFSMarking(grid, i, j - 1);
+}
+
+//LEETCODE 695
+ public int maxAreaOfIsland(int[][] grid) {
+        int max_area = 0;
+        for(int i = 0; i < grid.length; i++)
+            for(int j = 0; j < grid[0].length; j++)
+                if(grid[i][j] == 1)max_area = Math.max(max_area, AreaOfIsland(grid, i, j));
+        return max_area;
+    }
+    
+    public int AreaOfIsland(int[][] grid, int i, int j){
+        if( i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] == 1){
+            grid[i][j] = 0;
+            return 1 + AreaOfIsland(grid, i+1, j) + AreaOfIsland(grid, i-1, j) + AreaOfIsland(grid, i, j-1) + AreaOfIsland(grid, i, j+1);
+        }
+        return 0;
+    }
 
   public static void display(ArrayList < Edge > [] graph) {
     for (int i = 0; i < graph.length; i++) {
@@ -169,6 +168,23 @@ int numIslands(vector<vector<char>> &grid)
       System.out.println();
     }
   }
+
+//LEETCODE 463
+public int islandPerimeter(int[][] grid) {
+        int islands = 0, neighbours = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    islands++; // count islands
+                    if (i < grid.length - 1 && grid[i + 1][j] == 1) neighbours++; // count down neighbours
+                    if (j < grid[i].length - 1 && grid[i][j + 1] == 1) neighbours++; // count right neighbours
+                }
+            }
+        }
+
+        return islands * 4 - neighbours * 2;
+    }
 
   public static void main(String[] args) {
     for (int i = 0; i < graph.length; i++) {
